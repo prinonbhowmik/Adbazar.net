a3a146fa8e9ebcfbc198670140065b35cf85bf6f
Prinon <prinonarnob@gmail.com>
2021-03-31 17:38:03 +0600

    init works

---
+++ app/src/main/java/com/adbazarnet/Adapter/CategoryNamesAdapter.java
@@ -0,0 +1,42 @@
+package com.adbazarnet.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.adbazarnet.Models.CategoriesModel;
+import com.adbazarnet.R;
+
+import java.util.List;
+import java.util.Locale;
+
+public class CategoryNamesAdapter extends RecyclerView.Adapter<CategoryNamesAdapter.ViewHolder> {
+    private List<CategoriesModel> categoriesModels;
+    private Context context;
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.categories_recycler, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
