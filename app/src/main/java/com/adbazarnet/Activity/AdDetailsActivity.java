57fd63259fe40211dc800729b0c89c209fd68413
Prinon <prinonarnob@gmail.com>
2021-05-05 02:31:58 +0600

    dashboard done

--- app/src/main/java/com/adbazarnet/Activity/AdDetailsActivity.java
+++ app/src/main/java/com/adbazarnet/Activity/AdDetailsActivity.java
@@ -1,6 +1,9 @@
 package com.adbazarnet.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -10,6 +13,7 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,11 +22,15 @@ import android.widget.Toast;
 import com.adbazarnet.Adapter.ImageSliderAdapter;
 import com.adbazarnet.Adapter.RelatedProductAdapter;
 import com.adbazarnet.Api.ApiUtils;
+import com.adbazarnet.Fragments.FavouriteFragment;
+import com.adbazarnet.Fragments.HomeFragment;
 import com.adbazarnet.Models.AdDetails;
 import com.adbazarnet.Models.AdImages;
 import com.adbazarnet.Models.FavouriteAds;
 import com.adbazarnet.Models.RelatedAds;
+import com.adbazarnet.Models.UserDetailsModel;
 import com.adbazarnet.R;
+import com.google.android.material.navigation.NavigationView;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
@@ -36,7 +44,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class AdDetailsActivity extends AppCompatActivity {
+public class AdDetailsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private int id,userId;
     private SliderView imageSlider;
@@ -49,8 +57,9 @@ public class AdDetailsActivity extends AppCompatActivity {
     private RelatedProductAdapter relatedProductAdapter;
     private SharedPreferences sharedPreferences;
     private String token,userPhone;
-
+    private Dialog dialog;
     private ChipNavigationBar chipNavigationBar;
+    private int loggedIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +70,9 @@ public class AdDetailsActivity extends AppCompatActivity {
         id = intent.getIntExtra("id",0);
 
         init();
+
+        id = sharedPreferences.getInt("id",0);
+        loggedIn = sharedPreferences.getInt("loggedIn",0);
 
         Call<AdDetails> call = ApiUtils.getUserService().getAdDetails(id);
         call.enqueue(new Callback<AdDetails>() {
@@ -152,6 +164,119 @@ public class AdDetailsActivity extends AppCompatActivity {
             }
         });
 
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                switch (i){
+
+                    case R.id.home:
+                        FragmentTransaction home = getSupportFragmentManager().beginTransaction();
+                        home.replace(R.id.fragment_container, new HomeFragment());
+                        home.commit();
+                        break;
+                    case R.id.favourite:
+                        FragmentTransaction favourite = getSupportFragmentManager().beginTransaction();
+                        favourite.replace(R.id.fragment_container, new FavouriteFragment());
+                        favourite.commit();
+                        break;
+                    case R.id.adPost:
+                        break;
+                    case R.id.chat:
+                        Toast.makeText(MainActivity.this, "Chat", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.account:
+                        if (loggedIn == 0 ){
+                            startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                            finish();
+                            break;
+                        }else{
+                            //pop-up will be shown
+                            dialog = new Dialog(MainActivity.this);
+                            dialog.setContentView(R.layout.profile_option_xml);
+                            CardView close  = dialog.findViewById(R.id.closeTv);
+                            TextView dashboard = dialog.findViewById(R.id.dashboardTv);
+                            TextView myAds = dialog.findViewById(R.id.myAdsTv);
+                            TextView favouriteTv = dialog.findViewById(R.id.favouriteTv);
+                            TextView membership = dialog.findViewById(R.id.membershipTv);
+                            TextView profile = dialog.findViewById(R.id.profileTv);
+                            TextView logout = dialog.findViewById(R.id.logoutTv);
+
+                            close.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    dialog.dismiss();
+                                    finish();
+                                    startActivity(getIntent());
+                                }
+                            });
+
+                            myAds.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    startActivity(new Intent(MainActivity.this,MyAdsActivity.class));
+                                }
+                            });
+
+                            favouriteTv.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    chipNavigationBar.setItemSelected(R.id.favourite, true);
+                                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FavouriteFragment()).commit();
+                                    dialog.dismiss();
+                                }
+                            });
+
+                            dashboard.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    startActivity(new Intent(MainActivity.this,DashboardActivity.class));
+                                    finish();
+                                }
+                            });
+
+                            profile.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+                                    finish();
+                                }
+                            });
+
+                            logout.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Call<UserDetailsModel> call = ApiUtils.getUserService().logoutUser();
+                                    call.enqueue(new Callback<UserDetailsModel>() {
+                                        @Override
+                                        public void onResponse(Call<UserDetailsModel> call, Response<UserDetailsModel> response) {
+                                        }
+
+                                        @Override
+                                        public void onFailure(Call<UserDetailsModel> call, Throwable t) {
+
+                                        }
+                                    });
+                                    SharedPreferences sharedPreferences = getSharedPreferences("MyRef", MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putString("token", "");
+                                    editor.putInt("loggedIn", 0);
+                                    editor.putInt("id", 0);
+                                    editor.commit();
+                                    finish();
+                                    startActivity(getIntent());
+                                }
+                            });
+
+                            dialog.setCancelable(false);
+                            dialog.show();
+
+                            break;
+                        }
+                }
+
+            }
+        });
+
     }
 
     private void getRelatedProduct(List<RelatedAds> relatedAds) {
@@ -198,5 +323,10 @@ public class AdDetailsActivity extends AppCompatActivity {
         token = sharedPreferences.getString("token",null);
         userId = sharedPreferences.getInt("id",0);
         chipNavigationBar=findViewById(R.id.bottom_menu);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
