b98622d443bbd06e1fa51b0313c2c4318d0539ad
Prinon <prinonarnob@gmail.com>
2021-05-18 02:37:00 +0600

    post ad design

--- app/src/main/java/com/adbazarnet/Activity/PostAdActivity.java
+++ app/src/main/java/com/adbazarnet/Activity/PostAdActivity.java
@@ -2,17 +2,51 @@ package com.adbazarnet.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.adbazarnet.R;
 
 public class PostAdActivity extends AppCompatActivity {
 
+    private TextView locationTv,categoryTv;
+    private EditText titleEt,otherInfoEt,priceEt,phnnoEt1,phnnoEt2,phnnoEt3,descriptionEt;
+    private Switch negotiableBtn,hidePhnBtn;
+    private Button addPhnBtn,addImgBtn;
+    private ImageView img1,img2,img3,img4,img5;
+    private Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_ad);
+
+        init();
+
+    }
+
+    private void init() {
+        locationTv = findViewById(R.id.locationTv);
+        categoryTv = findViewById(R.id.categoryTv);
+        titleEt = findViewById(R.id.titleEt);
+        otherInfoEt = findViewById(R.id.otherInfoEt);
+        priceEt = findViewById(R.id.priceEt);
+        phnnoEt1 = findViewById(R.id.phnnoEt1);
+        phnnoEt2 = findViewById(R.id.phnnoEt2);
+        phnnoEt3 = findViewById(R.id.phnnoEt3);
+        descriptionEt = findViewById(R.id.descriptionEt);
+        addImgBtn = findViewById(R.id.addImgBtn);
+        addPhnBtn = findViewById(R.id.addPhnBtn);
+        img1 = findViewById(R.id.img1);
+        img2 = findViewById(R.id.img2);
+        img3 = findViewById(R.id.img3);
+        img4 = findViewById(R.id.img4);
+        img5 = findViewById(R.id.img5);
     }
 
     public void setNegotiable(View view) {
---
+++ app/src/main/java/com/adbazarnet/Models/PostAdModel.java
@@ -0,0 +1,80 @@
+package com.adbazarnet.Models;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class PostAdModel {
+    private String ad_title;
+    private String condition;
+    private String price;
+    private String warranty;
+    private String other_information;
+    private String description;
+    private String location;
+    private int category;
+    private boolean negotiable;
+    private String ad_type;
+    private boolean hide_phone;
+
+    public PostAdModel(String ad_title, String condition, String price, String warranty, String other_information, String description, String location, int category, boolean negotiable, String ad_type, boolean hide_phone) {
+        this.ad_title = ad_title;
+        this.condition = condition;
+        this.price = price;
+        this.warranty = warranty;
+        this.other_information = other_information;
+        this.description = description;
+        this.location = location;
+        this.category = category;
+        this.negotiable = negotiable;
+        this.ad_type = ad_type;
+        this.hide_phone = hide_phone;
+    }
+
+    public PostAdModel() {
+    }
+
+    public String getAd_title() {
+        return ad_title;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getWarranty() {
+        return warranty;
+    }
+
+    public String getOther_information() {
+        return other_information;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public boolean isNegotiable() {
+        return negotiable;
+    }
+
+    public String getAd_type() {
+        return ad_type;
+    }
+
+    public boolean isHide_phone() {
+        return hide_phone;
+    }
+}
